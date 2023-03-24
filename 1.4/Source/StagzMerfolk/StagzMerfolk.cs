bea5d556bb154f4b77a191aa7cde5967ec4122e0
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-03-24 10:02:18 +0100

    renamed namespace

--- 1.4/Source/StagzMerfolk/StagzMerfolk.cs
+++ 1.4/Source/StagzMerfolk/StagzMerfolk.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RimWorld;
-using Verse;
-using HarmonyLib;
+﻿using Verse;
 
-namespace MT
+namespace StagzMerfolk
 {
     [StaticConstructorOnStartup]
     public static class StagzMerfolk
