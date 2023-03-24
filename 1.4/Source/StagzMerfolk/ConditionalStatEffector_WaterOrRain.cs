edfe9c837170067e522c0d821205581f0ba6c0e4
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-03-24 14:58:39 +0100

    added Stagz_RainVeil

--- 1.4/Source/StagzMerfolk/ConditionalStatEffector_WaterOrRain.cs
+++ 1.4/Source/StagzMerfolk/ConditionalStatEffector_WaterOrRain.cs
@@ -18,11 +18,35 @@ public class ConditionalStatEffector_WaterOrRain : ConditionalStatAffecter
         if (!req.HasThing || pawn?.Map == null) return false;
 
         //Check if pawn is in water or in rain
-        return pawn.Position.GetTerrain(pawn.Map).IsWater || pawn.Map.weatherManager.RainRate > 0.01f;
+        return pawn.Position.GetTerrain(pawn.Map).IsWater || (!pawn.Position.Roofed(pawn.Map) && pawn.Map.weatherManager.RainRate > 0.01f);
     }
 
     public override string Label
     {
         get { return "StagzMerfolk_ConditionalStatEffector_WaterOrRain".Translate(); }
+    }
+}
+
+public class ConditionalStatEffector_Rain : ConditionalStatAffecter
+{
+    public override bool Applies(StatRequest req)
+    {
+        //Check if biotech is active
+        if (!ModsConfig.BiotechActive)
+        {
+            return false;
+        }
+
+        //Check if request is valid (request is for a pawn and pawn is on a map
+        var pawn = req.Thing as Pawn;
+        if (!req.HasThing || pawn?.Map == null) return false;
+
+        //Check if pawn is in rain
+        return pawn.Map.weatherManager.RainRate > 0.01f && !pawn.Position.Roofed(pawn.Map);
+    }
+
+    public override string Label
+    {
+        get { return "StagzMerfolk_ConditionalStatEffector_Rain".Translate(); }
     }
 }
