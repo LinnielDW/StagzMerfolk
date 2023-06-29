2aa8ede94f63a846bb3ace0cab0dd1b6a28f48fb
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-06-29 23:08:44 +0200

    added movement malus while on land for Stagz_SwiftSwim

--- 1.4/Source/StagzMerfolk/ConditionalStatEffectors/ConditionalStatEffector_WaterOrRain.cs
+++ 1.4/Source/StagzMerfolk/ConditionalStatEffectors/ConditionalStatEffector_WaterOrRain.cs
@@ -26,3 +26,26 @@ public class ConditionalStatEffector_WaterOrRain : ConditionalStatAffecter
         get { return "StagzMerfolk_ConditionalStatEffector_WaterOrRain".Translate(); }
     }
 }
+public class ConditionalStatEffector_NotWaterOrRain : ConditionalStatAffecter
+{
+    public override bool Applies(StatRequest req)
+    {
+        //Check if biotech is active
+        if (!ModsConfig.BiotechActive)
+        {
+            return false;
+        }
+
+        //Check if request is valid (request is for a pawn and pawn is on a map
+        var pawn = req.Thing as Pawn;
+        if (!req.HasThing || pawn?.Map == null) return false;
+
+        //Check if pawn is in water or in rain
+        return !pawn.IsWet();
+    }
+
+    public override string Label
+    {
+        get { return "StagzMerfolk_ConditionalStatEffector_NotWaterOrRain".Translate(); }
+    }
+}
