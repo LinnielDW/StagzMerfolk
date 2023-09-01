0cb7621aeb33b85211a78394d2110cd52b15807f
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-09-01 20:23:11 +0200

    added dbh compat and settings screen

---
+++ 1.4/Source/StagzMerfolk/HarmonyPatches/DbhHygieneCleanPatches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using DubsBadHygiene;
+using HarmonyLib;
+using Verse;
+
+namespace StagzMerfolk.HarmonyPatches;
+
+[HarmonyPatch]
+public class DbhHygieneCleanPatches
+{
+    [HarmonyPrepare]
+    private static bool shouldPatchBadHygene()
+    {
+        return ModsConfig.IsActive("dubwise.dubsbadhygiene");
+    }
+    
+    [HarmonyTargetMethod]
+    public static MethodInfo TargetMethod()
+    {
+        return AccessTools.Method("DubsBadHygiene.Need_Hygiene:clean");
+    }
+
+    [HarmonyPostfix]
+    private static void Postfix(float val, ref Pawn ___pawn)
+    {
+        if (___pawn?.genes != null && ___pawn.genes.HasGene(StagzDefOf.Stagz_Aquatic) && StagzMerfolkSettings.dbhCleaningCountsAsHydration)
+        {
+            ___pawn.needs.TryGetNeed(StagzDefOf.Stagz_NeedAquatic).CurLevel = Math.Min(___pawn.needs.TryGetNeed(StagzDefOf.Stagz_NeedAquatic).CurLevel + val, 1f);
+        }
+    }
+}
